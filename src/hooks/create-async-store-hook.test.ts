11811bdfcc29c0b5e8ffe29acd9fe38a49b91fb1
Clemens Akens <clebert@me.com>
2020-05-02 00:12:26 +0200

    Replace ironhook with batis

--- src/hooks/create-async-store-hook.test.ts
+++ src/hooks/create-async-store-hook.test.ts
@@ -1,48 +1,24 @@
-import {Subject} from 'ironhook';
+import {HookProcess} from 'batis';
 import defer from 'p-defer';
-import {
-  AsyncStore,
-  Snapshot,
-  createAsyncStoreHook,
-} from './create-async-store-hook';
+import {Snapshot, createAsyncStoreHook} from './create-async-store-hook';
 
-jest.mock('preact/hooks', () => require('ironhook'));
+jest.mock('preact/hooks', () => require('batis'));
 
 interface AsyncStorageMock {
   pullState: jest.Mock;
   pushState: jest.Mock;
 }
 
-interface ObserverMock {
-  next: jest.Mock;
-  error: jest.Mock;
-  complete: jest.Mock;
-}
-
 type State = string | undefined;
-
-function queueMacrotask(): Promise<void> {
-  const macrotask = defer<void>();
-
-  setTimeout(() => macrotask.resolve(), 0);
-
-  return macrotask.promise;
-}
-
-function queueMicrotask(): Promise<void> {
-  return Promise.resolve().then();
-}
 
 function reducer(previousState: State, action: string): State {
   return (previousState || '') + action;
 }
 
 const useAsyncStore = createAsyncStoreHook(reducer, 'd');
-const dispatch = expect.any(Function);
 
 describe('useAsyncStore()', () => {
   let asyncStorage: AsyncStorageMock;
-  let observer: ObserverMock;
   let pullState1: defer.DeferredPromise<Snapshot<State, number>>;
   let pullState2: defer.DeferredPromise<Snapshot<State, number>>;
   let pushState1: defer.DeferredPromise<Snapshot<State, number> | undefined>;
@@ -64,82 +40,95 @@ describe('useAsyncStore()', () => {
     asyncStorage.pushState
       .mockReturnValueOnce(pushState1.promise)
       .mockReturnValueOnce(pushState2.promise);
-
-    observer = {next: jest.fn(), error: jest.fn(), complete: jest.fn()};
-  });
-
-  describe('without async storage', () => {
-    test('initializing -> idle', async () => {
-      const subject = new Subject(() => useAsyncStore(undefined, 'x'));
-
-      subject.subscribe(observer);
-
-      await queueMacrotask();
-
-      expect(observer.next.mock.calls).toEqual([
-        [{readyState: 'initializing', state: 'x', dispatch}],
-      ]);
-
-      expect(observer.error.mock.calls).toEqual([]);
-      expect(observer.complete.mock.calls).toEqual([]);
-    });
   });
 
   test('initializing -> idle', async () => {
-    const subject = new Subject(() => useAsyncStore(asyncStorage, 'x'));
+    const {result, update} = HookProcess.start(useAsyncStore, [undefined, 'x']);
 
-    subject.subscribe(observer);
+    expect(result.getCurrent()).toEqual({
+      readyState: 'initializing',
+      state: 'x',
+      dispatch: expect.any(Function),
+    });
 
-    await queueMicrotask();
+    expect(update([asyncStorage, 'x'])).toEqual({
+      readyState: 'initializing',
+      state: 'x',
+      dispatch: expect.any(Function),
+    });
 
     pullState1.resolve({state: 'a', version: 1});
 
-    await queueMacrotask();
-
-    expect(asyncStorage.pullState.mock.calls).toEqual([['d']]);
-    expect(asyncStorage.pushState.mock.calls).toEqual([]);
-
-    expect(observer.next.mock.calls).toEqual([
-      [{readyState: 'initializing', state: 'x', dispatch}],
-      [{readyState: 'idle', state: 'a', dispatch}],
-    ]);
-
-    expect(observer.error.mock.calls).toEqual([]);
-    expect(observer.complete.mock.calls).toEqual([]);
+    await expect(result.getNextAsync()).resolves.toEqual({
+      readyState: 'idle',
+      state: 'a',
+      dispatch: expect.any(Function),
+    });
   });
 
   test('initializing -> idle -> synchronizing -> idle', async () => {
-    let asyncStore!: AsyncStore<State, string>;
+    const {result} = HookProcess.start(useAsyncStore, [asyncStorage, 'x']);
+    const {dispatch} = result.getCurrent();
 
-    const subject = new Subject(
-      () => (asyncStore = useAsyncStore(asyncStorage, 'x'))
-    );
+    expect(result.getCurrent()).toEqual({
+      readyState: 'initializing',
+      state: 'x',
+      dispatch: expect.any(Function),
+    });
 
-    subject.subscribe(observer);
+    dispatch('y');
 
-    asyncStore.dispatch('y');
+    await expect(result.getNextAsync()).resolves.toEqual({
+      readyState: 'initializing',
+      state: 'xy',
+      dispatch: expect.any(Function),
+    });
 
-    await queueMicrotask();
-
+    expect(asyncStorage.pullState).toHaveBeenCalledTimes(1);
     pullState1.resolve({state: 'a', version: 1});
 
-    await queueMicrotask();
+    await expect(result.getNextAsync()).resolves.toEqual({
+      readyState: 'synchronizing',
+      state: 'ay',
+      dispatch: expect.any(Function),
+    });
 
-    asyncStore.dispatch('b');
+    dispatch('b');
 
+    await expect(result.getNextAsync()).resolves.toEqual({
+      readyState: 'synchronizing',
+      state: 'ayb',
+      dispatch: expect.any(Function),
+    });
+
+    expect(asyncStorage.pushState).toHaveBeenCalledTimes(1);
     pushState1.resolve(undefined);
 
-    await queueMicrotask();
+    dispatch('c');
 
-    asyncStore!.dispatch('c');
+    await expect(result.getNextAsync()).resolves.toEqual({
+      readyState: 'synchronizing',
+      state: 'aybc',
+      dispatch: expect.any(Function),
+    });
 
+    expect(asyncStorage.pullState).toHaveBeenCalledTimes(2);
     pullState2.resolve({state: 'z', version: 2});
 
-    await queueMicrotask();
+    await expect(result.getNextAsync()).resolves.toEqual({
+      readyState: 'synchronizing',
+      state: 'zybc',
+      dispatch: expect.any(Function),
+    });
 
+    expect(asyncStorage.pushState).toHaveBeenCalledTimes(2);
     pushState2.resolve({state: 'zybc!', version: 3});
 
-    await queueMacrotask();
+    await expect(result.getNextAsync()).resolves.toEqual({
+      readyState: 'idle',
+      state: 'zybc!',
+      dispatch: expect.any(Function),
+    });
 
     expect(asyncStorage.pullState.mock.calls).toEqual([['d'], ['d']]);
 
@@ -147,77 +136,36 @@ describe('useAsyncStore()', () => {
       ['ay', 1],
       ['zybc', 2],
     ]);
-
-    expect(observer.next.mock.calls).toEqual([
-      [{readyState: 'initializing', state: 'x', dispatch}],
-      [{readyState: 'initializing', state: 'xy', dispatch}],
-      [{readyState: 'synchronizing', state: 'ay', dispatch}],
-      [{readyState: 'synchronizing', state: 'ay', dispatch}],
-      [{readyState: 'synchronizing', state: 'ayb', dispatch}],
-      [{readyState: 'synchronizing', state: 'aybc', dispatch}],
-      [{readyState: 'synchronizing', state: 'zybc', dispatch}],
-      [{readyState: 'synchronizing', state: 'zybc', dispatch}],
-      [{readyState: 'idle', state: 'zybc!', dispatch}],
-    ]);
-
-    expect(observer.error.mock.calls).toEqual([]);
-    expect(observer.complete.mock.calls).toEqual([]);
   });
 
   test('failing pullState()', async () => {
-    const subject = new Subject(() => useAsyncStore(asyncStorage, 'x'));
-
-    subject.subscribe(observer);
-
-    await queueMicrotask();
+    const {result} = HookProcess.start(useAsyncStore, [asyncStorage, 'x']);
 
     pullState1.reject(new Error('Oops!'));
 
-    await queueMacrotask();
-
     expect(asyncStorage.pullState.mock.calls).toEqual([['d']]);
     expect(asyncStorage.pushState.mock.calls).toEqual([]);
-
-    expect(observer.next.mock.calls).toEqual([
-      [{readyState: 'initializing', state: 'x', dispatch}],
-    ]);
-
-    expect(observer.error.mock.calls).toEqual([[new Error('Oops!')]]);
-    expect(observer.complete.mock.calls).toEqual([]);
+    await expect(result.getNextAsync()).rejects.toEqual(new Error('Oops!'));
   });
 
   test('failing pushState()', async () => {
-    let asyncStore!: AsyncStore<State, string>;
-
-    const subject = new Subject(
-      () => (asyncStore = useAsyncStore(asyncStorage, 'x'))
-    );
-
-    subject.subscribe(observer);
-
-    await queueMicrotask();
+    const {result} = HookProcess.start(useAsyncStore, [asyncStorage, 'x']);
+    const {dispatch} = result.getCurrent();
 
     pullState1.resolve({state: 'a', version: 1});
 
-    await queueMicrotask();
+    dispatch('b');
 
-    asyncStore.dispatch('b');
+    await expect(result.getNextAsync()).resolves.toEqual({
+      readyState: 'synchronizing',
+      state: 'ab',
+      dispatch: expect.any(Function),
+    });
 
     pushState1.reject(new Error('Oops!'));
 
-    await queueMacrotask();
-
     expect(asyncStorage.pullState.mock.calls).toEqual([['d']]);
     expect(asyncStorage.pushState.mock.calls).toEqual([['ab', 1]]);
-
-    expect(observer.next.mock.calls).toEqual([
-      [{readyState: 'initializing', state: 'x', dispatch}],
-      [{readyState: 'idle', state: 'a', dispatch}],
-      [{readyState: 'synchronizing', state: 'ab', dispatch}],
-      [{readyState: 'synchronizing', state: 'ab', dispatch}],
-    ]);
-
-    expect(observer.error.mock.calls).toEqual([[new Error('Oops!')]]);
-    expect(observer.complete.mock.calls).toEqual([]);
+    await expect(result.getNextAsync()).rejects.toEqual(new Error('Oops!'));
   });
 });
